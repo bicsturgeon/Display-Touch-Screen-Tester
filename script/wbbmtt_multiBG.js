53852324e6450b8a0c524e795c491623e02ba614
bicsturgeon <48686519+bicsturgeon@users.noreply.github.com>
2020-08-24 17:02:29 +0700

    Update wbbmtt_multiBG.js
    Change 3 fingers to 6 fingers to change background color.

--- script/wbbmtt_multiBG.js
+++ script/wbbmtt_multiBG.js
@@ -409,7 +409,7 @@
 				init = false;
                 this._drawTouchPoint(ctx, touch, touchColor(i));
 				//if (touch == "touchend") {
-					if(i >= 2){
+					if(i >= 5){
 						bg++;
 					}
 				//}
